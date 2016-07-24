13fe446d7b100e1d68e453914eef6c033b086ab2
kangsafe <kangsafe@hotmail.com>
2016-07-24 23:43:08 +0800

    打包备份

---
+++ Me.Task.Ctrl/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Me.Task.Ctrl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.tbSave = new System.Windows.Forms.ToolStripTextBox();
+            this.btnSave = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnAdd = new System.Windows.Forms.ToolStripButton();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.gv = new System.Windows.Forms.DataGridView();
+            this.backsource = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.savepath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.taskstatus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.begintime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.endtime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pbar = new System.Windows.Forms.ProgressBar();
+            this.btnStart = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel2,
+            this.tbSave,
+            this.btnSave,
+            this.toolStripSeparator2,
+            this.btnAdd,
+            this.btnStart});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(685, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(56, 22);
+            this.toolStripLabel2.Text = "保存路径";
+            // 
+            // tbSave
+            // 
+            this.tbSave.Name = "tbSave";
+            this.tbSave.Size = new System.Drawing.Size(100, 25);
+            this.tbSave.ToolTipText = "保存目录";
+            // 
+            // btnSave
+            // 
+            this.btnSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
+            this.btnSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(60, 22);
+            this.btnSave.Text = "选择目录";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnAdd.Image = ((System.Drawing.Image)(resources.GetObject("btnAdd.Image")));
+            this.btnAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(60, 22);
+            this.btnAdd.Text = "添加备份";
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // gv
+            // 
+            this.gv.AllowUserToOrderColumns = true;
+            this.gv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.backsource,
+            this.savepath,
+            this.taskstatus,
+            this.begintime,
+            this.endtime});
+            this.gv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gv.Location = new System.Drawing.Point(0, 25);
+            this.gv.Name = "gv";
+            this.gv.RowTemplate.Height = 23;
+            this.gv.Size = new System.Drawing.Size(685, 236);
+            this.gv.TabIndex = 1;
+            // 
+            // backsource
+            // 
+            this.backsource.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.backsource.FillWeight = 40F;
+            this.backsource.HeaderText = "路径";
+            this.backsource.Name = "backsource";
+            // 
+            // savepath
+            // 
+            this.savepath.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.savepath.FillWeight = 40F;
+            this.savepath.HeaderText = "保存位置";
+            this.savepath.Name = "savepath";
+            // 
+            // taskstatus
+            // 
+            this.taskstatus.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.taskstatus.FillWeight = 4F;
+            this.taskstatus.HeaderText = "任务状态";
+            this.taskstatus.Name = "taskstatus";
+            // 
+            // begintime
+            // 
+            this.begintime.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.begintime.FillWeight = 8F;
+            this.begintime.HeaderText = "开始时间";
+            this.begintime.Name = "begintime";
+            // 
+            // endtime
+            // 
+            this.endtime.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.endtime.FillWeight = 8F;
+            this.endtime.HeaderText = "结束时间";
+            this.endtime.Name = "endtime";
+            // 
+            // pbar
+            // 
+            this.pbar.Location = new System.Drawing.Point(180, 89);
+            this.pbar.Name = "pbar";
+            this.pbar.Size = new System.Drawing.Size(302, 23);
+            this.pbar.TabIndex = 2;
+            this.pbar.Visible = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnStart.Image = ((System.Drawing.Image)(resources.GetObject("btnStart.Image")));
+            this.btnStart.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(60, 22);
+            this.btnStart.Text = "开始执行";
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 261);
+            this.Controls.Add(this.pbar);
+            this.Controls.Add(this.gv);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripTextBox tbSave;
+        private System.Windows.Forms.ToolStripButton btnSave;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton btnAdd;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.DataGridView gv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn backsource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn savepath;
+        private System.Windows.Forms.DataGridViewTextBoxColumn taskstatus;
+        private System.Windows.Forms.DataGridViewTextBoxColumn begintime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn endtime;
+        private System.Windows.Forms.ProgressBar pbar;
+        private System.Windows.Forms.ToolStripButton btnStart;
+    }
+}
+
---
+++ Me.Task.Ctrl/Form1.cs
@@ -0,0 +1,151 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Me.Task.Ctrl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pbar.Minimum = 0;
+            pbar.Maximum = 100;
+            pbar.Hide();
+        }
+        //选择保存位置
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tbSave.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+        //添加备份计划目录
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                //路径
+                DataGridViewTextBoxCell path = new DataGridViewTextBoxCell();
+                path.Value = folderBrowserDialog1.SelectedPath;
+                row.Cells.Add(path);
+                //保存文件名
+                DataGridViewTextBoxCell name = new DataGridViewTextBoxCell();
+                name.Value = "";
+                row.Cells.Add(name);
+                //状态
+                DataGridViewTextBoxCell px = new DataGridViewTextBoxCell();
+                px.Value = "未开始";
+                row.Cells.Add(px);
+                //开始时间
+                DataGridViewTextBoxCell deep = new DataGridViewTextBoxCell();
+                deep.Value = "";
+                row.Cells.Add(deep);
+                //结束时间
+                DataGridViewTextBoxCell deep1 = new DataGridViewTextBoxCell();
+                deep1.Value = "";
+                row.Cells.Add(deep1);
+                gv.Rows.Add(row);
+            }
+        }
+        //开始执行
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            pbar.Minimum = 0;
+            pbar.Maximum = gv.Rows.Count;
+            pbar.Value = 0;
+            pbar.Show();
+            //保存路径
+            if (Directory.Exists(tbSave.Text))
+            {
+                Directory.CreateDirectory(tbSave.Text);
+            }
+            for (int r = 0; r < gv.Rows.Count - 1; r++)
+            {
+                string path = tbSave.Text + "\\" + new Guid().ToString().Replace("-", "").ToLower() + ".zip";
+                gv.Rows[r].Cells[1].Value = path;
+                gv.Rows[r].Cells[3].Value = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+                if (File.Exists(path))
+                {
+                    gv.Rows[r].Cells[2].Value = "失败:已存在该文件";
+                    gv.Rows[r].Cells[4].Value = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+                }
+                else
+                {
+                    byte[] buffer = ZipFolder(gv.Rows[r].Cells[0].Value.ToString());
+                    File.WriteAllBytes(path, buffer);
+                    gv.Rows[r].Cells[2].Value = "成功";
+                    gv.Rows[r].Cells[4].Value = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+                }
+            }
+            pbar.Hide();
+        }
+        /// <summary>
+        /// 压缩打包文件
+        /// </summary>
+        private byte[] ZipFolder(string zips)
+        {
+            MemoryStream ms = new MemoryStream();
+            byte[] buffer = null;
+
+            using (ZipFile file = ZipFile.Create(ms))
+            {
+                file.BeginUpdate();
+                //file.NameTransform = new MyNameTransfom();//通过这个名称格式化器，可以将里面的文件名进行一些处理。默认情况下，会自动根据文件的路径在zip中创建有关的文件夹。
+                foreach (var m in zips.videos)
+                {
+                    if (!string.IsNullOrEmpty(m.sqeeze) && File.Exists(Server.MapPath("/ResV1_0/Video/" + m.sqeeze + ".mp4")))
+                    {
+                        file.Add(Server.MapPath("/ResV1_0/Video/" + m.sqeeze + ".mp4"));
+                    }
+                    if (!string.IsNullOrEmpty(m.sqeezehold) && System.IO.File.Exists(Server.MapPath("/ResV1_0/Video/" + m.sqeezehold + ".mp4")))
+                    {
+                        file.Add(Server.MapPath("/ResV1_0/Video/" + m.sqeezehold + ".mp4"));
+                    }
+                    if (!string.IsNullOrEmpty(m.relax) && System.IO.File.Exists(Server.MapPath("/ResV1_0/Video/" + m.relax + ".mp4")))
+                    {
+                        file.Add(Server.MapPath("/ResV1_0/Video/" + m.relax + ".mp4"));
+                    }
+                    if (!string.IsNullOrEmpty(m.relaxhold) && System.IO.File.Exists(Server.MapPath("/ResV1_0/Video/" + m.relaxhold + ".mp4")))
+                    {
+                        file.Add(Server.MapPath("/ResV1_0/Video/" + m.relaxhold + ".mp4"));
+                    }
+                }
+                foreach (var m in zips.tails)
+                {
+                    if (!string.IsNullOrEmpty(m.fname) && System.IO.File.Exists(Server.MapPath("/ResV1_0/Tail/" + m.fname + ".mp4")))
+                    {
+                        file.Add(Server.MapPath("/ResV1_0/Tail/" + m.fname + ".mp4"));
+                    }
+                }
+
+                foreach (var m in zips.tips)
+                {
+                    if (!string.IsNullOrEmpty(m.tips) && System.IO.File.Exists(Server.MapPath("/ResV1_0/Tips/" + m.tips + ".mp3")))
+                    {
+                        file.Add(Server.MapPath("/ResV1_0/Tips/" + m.tips + ".mp3"));
+                    }
+                }
+                file.CommitUpdate();
+
+                buffer = new byte[ms.Length];
+                ms.Position = 0;
+                ms.Read(buffer, 0, buffer.Length);
+            }
+
+            return buffer;
+        }
+
+    }
+
+}
